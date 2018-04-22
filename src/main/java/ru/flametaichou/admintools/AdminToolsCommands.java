5f8de766ceae57c44f2004bad3ab927bb810be74
Layne Adama <lein.flamesword@gmail.com>
2018-04-22 21:07:44 +0300

    regen

--- src/main/java/ru/flametaichou/admintools/AdminToolsCommands.java
+++ src/main/java/ru/flametaichou/admintools/AdminToolsCommands.java
@@ -3,6 +3,7 @@ package ru.flametaichou.admintools;
 import java.util.ArrayList;
 import java.util.List;
 
+import net.minecraft.block.Block;
 import net.minecraft.command.CommandBase;
 import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.Entity;
@@ -13,7 +14,12 @@ import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.AxisAlignedBB;
 import net.minecraft.util.ChatComponentText;
 import net.minecraft.util.ChatComponentTranslation;
+import net.minecraft.world.ChunkCoordIntPair;
 import net.minecraft.world.World;
+import net.minecraft.world.WorldServer;
+import net.minecraft.world.chunk.Chunk;
+import net.minecraft.world.chunk.IChunkProvider;
+import net.minecraft.world.gen.ChunkProviderServer;
 
 public class AdminToolsCommands extends CommandBase
 { 
@@ -63,13 +69,13 @@ public class AdminToolsCommands extends CommandBase
         World world = sender.getEntityWorld(); 
     
         if (!world.isRemote) {
-            if(argString.length == 0) {
+            if (argString.length == 0) {
                 sender.addChatMessage(new ChatComponentText("/admin <mobclear (mob, range) / chestclear (range)>"));
-                return; 
+                return;
             }
             if (argString[0].equals("mobclear")) {
-	            if(sender instanceof EntityPlayer) {
-	            	EntityPlayer player = (EntityPlayer) sender;
+                if (sender instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) sender;
                     if (argString.length < 2) {
                         sender.addChatMessage(new ChatComponentTranslation("mobclear.noname"));
                         return;
@@ -87,7 +93,7 @@ public class AdminToolsCommands extends CommandBase
                         sender.addChatMessage(new ChatComponentTranslation("mobclear.range50"));
                         radius = 50;
                     }
-                    List e = player.worldObj.getEntitiesWithinAABB(EntityLiving.class, AxisAlignedBB.getBoundingBox(player.posX-radius, player.posY-radius, player.posZ-radius, (player.posX + radius),(player.posY + radius),(player.posZ + radius)));
+                    List e = player.worldObj.getEntitiesWithinAABB(EntityLiving.class, AxisAlignedBB.getBoundingBox(player.posX - radius, player.posY - radius, player.posZ - radius, (player.posX + radius), (player.posY + radius), (player.posZ + radius)));
                     if (e.size() > 0) {
                         int count = 0;
                         for (Object obj : e) {
@@ -99,14 +105,14 @@ public class AdminToolsCommands extends CommandBase
                         }
                         sender.addChatMessage(new ChatComponentTranslation("mobclear.done", count));
                     }
-	            }
-	            return;
-	        }
-        
+                }
+                return;
+            }
+
             if (argString[0].equals("chestclear")) {
-	            if(sender instanceof EntityPlayer) {
-	            	EntityPlayer player = (EntityPlayer)sender;
-	            	int player_x = (int) player.posX;
+                if (sender instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) sender;
+                    int player_x = (int) player.posX;
                     int player_y = (int) player.posY;
                     int player_z = (int) player.posZ;
                     if (argString.length < 2) {
@@ -143,9 +149,57 @@ public class AdminToolsCommands extends CommandBase
                         }
                     }
                     sender.addChatMessage(new ChatComponentTranslation("chestclear.done", count));
-	            }
-	            return;
-	        }
+                }
+                return;
+            }
+
+            if (argString[0].equals("chunkregen")) {
+                if (sender instanceof EntityPlayer) {
+                    EntityPlayer player = (EntityPlayer) sender;
+                    try {
+//                        ChunkProviderServer chunkServer = (ChunkProviderServer) world.getChunkProvider();
+//
+//                        List<ChunkCoordIntPair> toUnload = new ArrayList<ChunkCoordIntPair>();
+//                        for (Object obj : chunkServer.loadedChunks) {
+//                            Chunk chunk = (Chunk) obj;
+//                            toUnload.add(chunk.getChunkCoordIntPair());
+//                        }
+//
+//                        for (ChunkCoordIntPair pair : toUnload) {
+//                        }
+
+                        Chunk oldChunk = world.getChunkFromChunkCoords(player.chunkCoordX, player.chunkCoordZ);
+                        WorldServer worldServer = (WorldServer) world;
+                        ChunkProviderServer chunkProviderServer = worldServer.theChunkProviderServer;
+//                IChunkProvider chunkProviderGenerate = ObfuscationReflectionHelper.getPrivateValue(ChunkProviderServer.class, chunkProviderServer, "d", "field_73246_d");
+                        IChunkProvider chunkProviderGenerate = chunkProviderServer.currentChunkProvider;
+
+                        Chunk newChunk = chunkProviderGenerate.provideChunk(oldChunk.xPosition, oldChunk.zPosition);
+
+                        for (int x = 0; x < 16; x++) {
+                            for (int z = 0; z < 16; z++) {
+                                for (int y = 0; y < world.getHeight(); y++) {
+                                    Block blockID = newChunk.getBlock(x, y, z);
+                                    int metadata = newChunk.getBlockMetadata(x, y, z);
+                                    worldServer.setBlock(x + oldChunk.xPosition * 16, y, z + oldChunk.zPosition * 16, blockID, metadata, 2);
+                                    TileEntity tileEntity = newChunk.getTileEntityUnsafe(x, y, z);
+                                    if (tileEntity != null) {
+                                        worldServer.setTileEntity(x + oldChunk.xPosition * 16, y, z + oldChunk.zPosition * 16, tileEntity);
+                                    }
+                                }
+                            }
+                        }
+
+                        oldChunk.isTerrainPopulated = false;
+                        chunkProviderGenerate.populate(chunkProviderGenerate, oldChunk.xPosition, oldChunk.zPosition);
+                        sender.addChatMessage(new ChatComponentTranslation("chunkregen.done"));
+                    } catch (Exception e) {
+                        sender.addChatMessage(new ChatComponentTranslation("chunkregen.error"));
+                        e.printStackTrace();
+                    }
+                }
+                return;
+            }
         }
     } 
 
